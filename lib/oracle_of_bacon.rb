cf10fbc836c2a4832b96df8e4c7de7928bebf3f3
EC2 Default User <ec2-user@ip-172-31-13-85.ec2.internal>
2019-02-27 16:05:18 +0000

    add functionality

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -20,11 +20,15 @@ class OracleOfBacon
   validate :from_does_not_equal_to
 
   def from_does_not_equal_to
-    # YOUR CODE HERE
+    if @from == @to
+      errors.add(:from_equals_to, "From cannot be the same as To")
+    end
   end
 
-  def initialize(api_key='')
-    # your code here
+  def initialize(api_key='38b99ce9ec87')
+    @api_key = api_key
+    @from = "Kevin Bacon"
+    @to = "Kevin Bacon"
   end
 
   def find_connections
@@ -37,13 +41,16 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      raise OracleOfBacon::NetworkError, e
     end
     # your code here: create the OracleOfBacon::Response object
+    @response = Response.new(xml)
   end
 
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+    @uri = "http://oracleofbacon.org/cgi-bin/xml?p=#{@api_key}&a=#{CGI.escape(@from)}&b=#{CGI.escape(@to)}"
   end
       
   class Response
@@ -61,9 +68,26 @@ class OracleOfBacon
         parse_error_response
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      # object should have type 'unknown' and data 'unknown response' 
+      elsif ! @doc.xpath('/link').empty?
+        @type = :graph
+        parse_link_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        @type = :spellcheck
+        @data = @doc.xpath('//match').map {|node| node.text}
+      else
+        @type = :unknown
+        @data = 'unknown response type'
       end
     end
+    
+    def parse_link_response
+      actors = @doc.xpath('//actor').map {|node| node.text}
+      movies = @doc.xpath('//movie').map {|node| node.text}
+      @data = actors.zip(movies).flatten.compact
+    end
+      
+    
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
